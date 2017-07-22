912416e8350ed0428daec9a9fbc2dc987e5634b1
sstocki <sylwester.stocki.rnd@gmail.com>
2017-07-22 22:30:40 +0100

    AdventOfCode: Day3 - SquaresWithThreeSides - solution to both tasks.

---
+++ src/test/scala/silverstar/adventofcode/day3/SquaresWithThreeSidesSpec.scala
@@ -0,0 +1,36 @@
+package silverstar.adventofcode.day3
+
+import org.scalatest.{FunSpec, Matchers}
+
+import scala.io.Source
+
+class SquaresWithThreeSidesSpec extends FunSpec with Matchers {
+
+  import SquaresWithThreeSides._
+
+  describe("isTriangle") {
+    it("should return true for valid triangle") {
+      isTriangle((2, 3, 4)) shouldBe true
+    }
+
+    it("should return false for invalid triangle") {
+      isTriangle((10, 15, 25)) shouldBe false
+    }
+  }
+
+  describe("howManyTriangles") {
+    it("should return result for example data") {
+      val input = getClass.getResourceAsStream("SquaresWithThreeSides.txt")
+      val data = Source.fromInputStream(input, "UTF8").mkString
+      howManyTriangles(data) shouldBe 983
+    }
+  }
+
+  describe("howManyTrianglesVertical") {
+    it("should return result for example data") {
+      val input = getClass.getResourceAsStream("SquaresWithThreeSides.txt")
+      val data = Source.fromInputStream(input, "UTF8").mkString
+      howManyTrianglesVertical(data) shouldBe 1836
+    }
+  }
+}
