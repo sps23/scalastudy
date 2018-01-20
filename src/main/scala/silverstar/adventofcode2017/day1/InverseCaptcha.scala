a1e505e420a7fa6f1e3aa49b490a73d168edc9de
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2018-01-20 12:30:14 +0000

    AdventOfCode2017 Day1: InverseCaptcha - solution to task2.

--- src/main/scala/silverstar/adventofcode2017/day1/InverseCaptcha.scala
+++ src/main/scala/silverstar/adventofcode2017/day1/InverseCaptcha.scala
@@ -16,16 +16,32 @@ import scala.annotation.tailrec
   * in the list.
   * *
   * For example:
-  * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
+  * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit
+  * and the third digit (2) matches the fourth digit.
   * 1111 produces 4 because each digit (all 1) matches the next.
   * 1234 produces 0 because no digit matches the next.
   * 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
   *
-  * What is the solution to your captcha
+  * --- Part Two ---
+  * You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied,
+  * but it did emit a star as encouragement. The instructions change:
+  * *
+  * Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list.
+  * That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+  * matches it. Fortunately, your list has an even number of elements.
+  * *
+  * For example:
+  * *
+  * 1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+  * 1221 produces 0, because every comparison is between a 1 and a 2.
+  * 123425 produces 4, because both 2s match each other, but no other digit has a match.
+  * 123123 produces 12.
+  * 12131415 produces 4.
+  * What is the solution to your new captcha?
   */
 object InverseCaptcha {
 
-  def solveCaptcha(captcha: String): Long = {
+  def solveCaptcha1(captcha: String): Long = {
 
     @tailrec
     def iter(digits: List[Long], acc: Long): Long = digits match {
@@ -51,5 +67,15 @@ object InverseCaptcha {
     }
 
     iter(captcha.toList :+ captcha.head, 0)
+  }
+
+  def solveNewCaptcha(captcha: String): Long = {
+    val (firstHalf, secondHalf) = captcha.toList.splitAt(captcha.length / 2)
+    val zipped = captcha.toList.zip(secondHalf ++ firstHalf)
+
+    zipped.foldLeft(0)((acc, pair) => {
+      if(pair._1 == pair._2) acc + pair._1.asDigit
+      else acc
+    })
   }
 }
--- src/test/scala/silverstar/adventofcode2017/day1/InverseCaptchaSpec.scala
+++ src/test/scala/silverstar/adventofcode2017/day1/InverseCaptchaSpec.scala
@@ -8,40 +8,65 @@ class InverseCaptchaSpec extends FunSpec with Matchers {
 
   import silverstar.adventofcode2017.day1.InverseCaptcha._
 
-  val testData: Seq[(String, Long)] = Seq(
-    ("1122", 3),
-    ("1111", 4),
-    ("1234", 0),
-    ("91212129", 9)
-  )
 
   describe("solveCaptcha") {
+    val testData: Seq[(String, Long)] = Seq(
+      ("1122", 3),
+      ("1111", 4),
+      ("1234", 0),
+      ("91212129", 9)
+    )
 
-    testData.foreach(t => {
-      it(s"should return '${t._2}' for '${t._1}'") {
-        solveCaptcha(t._1) shouldBe t._2
+    describe("solveCaptcha1") {
+
+      testData.foreach(t => {
+        it(s"should return '${t._2}' for '${t._1}'") {
+          solveCaptcha1(t._1) shouldBe t._2
+        }
+      })
+
+      it("should return result for example data") {
+        val input = getClass.getResourceAsStream("InverseCaptcha.txt")
+        val data = Source.fromInputStream(input, "UTF8").mkString
+        solveCaptcha1(data) shouldBe 1341
       }
-    })
+    }
 
-    it("should return result for example data") {
-      val input = getClass.getResourceAsStream("InverseCaptcha.txt")
-      val data = Source.fromInputStream(input, "UTF8").mkString
-      solveCaptcha(data) shouldBe 1341
+    describe("solveCaptcha2") {
+
+      testData.foreach(t => {
+        it(s"should return '${t._2}' for '${t._1}'") {
+          solveCaptcha2(t._1) shouldBe t._2
+        }
+      })
+
+      it("should return result for example data") {
+        val input = getClass.getResourceAsStream("InverseCaptcha.txt")
+        val data = Source.fromInputStream(input, "UTF8").mkString
+        solveCaptcha2(data) shouldBe 1341
+      }
     }
   }
 
-  describe("solveCaptcha2") {
+  describe("solveNewCaptcha") {
+    val testData: Seq[(String, Long)] = Seq(
+      ("1212", 6),
+      ("1221", 0),
+      ("123425", 4),
+      ("123123", 12),
+      ("12131415", 4)
+    )
 
     testData.foreach(t => {
       it(s"should return '${t._2}' for '${t._1}'") {
-        solveCaptcha2(t._1) shouldBe t._2
+        solveNewCaptcha(t._1) shouldBe t._2
       }
     })
 
     it("should return result for example data") {
       val input = getClass.getResourceAsStream("InverseCaptcha.txt")
       val data = Source.fromInputStream(input, "UTF8").mkString
-      solveCaptcha2(data) shouldBe 1341
+      solveNewCaptcha(data) shouldBe 1348
     }
   }
 }
