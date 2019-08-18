ebab4554976c7c9762f1882f0d4fb8fc0a64c273
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2019-08-19 00:25:16 +0100

    Apache Camel Jetty two endpoints on the same port.

--- src/main/scala/silverstar/cameljettytest/CamelJettyApp.scala
+++ src/main/scala/silverstar/cameljettytest/CamelJettyApp.scala
@@ -4,7 +4,10 @@ import org.apache.camel.main.Main
 
 object CamelJettyApp extends App {
 
+  private val defaultPort = "9820"
+
   val camelMain = new Main
-  camelMain.addRouteBuilder(new CamelJettyRoute)
+  camelMain.addRouteBuilder(new CamelJettyRoute(defaultPort, "test1"))
+  camelMain.addRouteBuilder(new CamelJettyRoute(defaultPort, "test2"))
   camelMain.run()
 }
--- src/main/scala/silverstar/cameljettytest/CamelJettyRoute.scala
+++ src/main/scala/silverstar/cameljettytest/CamelJettyRoute.scala
@@ -2,9 +2,9 @@ package silverstar.cameljettytest
 
 import org.apache.camel.builder.RouteBuilder
 
-class CamelJettyRoute extends RouteBuilder {
+class CamelJettyRoute(port: String, name: String) extends RouteBuilder {
 
   override def configure(): Unit = {
-    from("jetty:http://localhost:9080/testservice").process(new HttpProcessor)
+    from(s"jetty:http://localhost:$port/$name").process(new HttpProcessor)
   }
 }
