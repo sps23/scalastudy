ed880439563b15e318849e3f863e0681cae1d35c
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2019-12-15 21:00:29 +0000

    AoC_2019_D1: TheTyrannyOfTheRocketEquation part2.

--- src/main/scala/silverstar/adventofcode2019/day1/TheTyrannyOfTheRocketEquation.scala
+++ src/main/scala/silverstar/adventofcode2019/day1/TheTyrannyOfTheRocketEquation.scala
@@ -2,5 +2,19 @@ package silverstar.adventofcode2019.day1
 
 object TheTyrannyOfTheRocketEquation {
 
-  def sumOfFuelRequirements(mass: Seq[Long]): Long = mass.foldLeft(0L)((acc, m) => acc + (m / 3) - 2)
+  private def fuelRequired(mass: Long): Long = (mass / 3) - 2
+
+  def sumOfFuelRequirements(mass: Seq[Long]): Long = mass.foldLeft(0L)((acc, m) => acc + fuelRequired(m))
+
+  def sumOfFuelRequirementsCorrection(mass: Seq[Long]): Long = {
+
+    @scala.annotation.tailrec
+    def iter(m: Long, sum: Long): Long = {
+      val fuelReq: Long = fuelRequired(m)
+      if (fuelReq <= 0) sum
+      else iter(fuelReq, sum + fuelReq)
+    }
+
+    mass.foldLeft(0L)((acc, m) => acc + iter(m, sum = 0L))
+  }
 }
